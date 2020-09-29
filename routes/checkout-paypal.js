0e8e564b28ba9be0737bf7329b1e4654fbfe6828
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-09-29 15:01:12 +0100

    validate form inputs from backend on checkout

--- routes/checkout-paypal.js
+++ routes/checkout-paypal.js
@@ -20,6 +20,12 @@ router.post("/create-payment", async (req, res) => {
     })).reduce((sum, p) => sum + (p.price * p.quantity), 0);
 
     try {
+        const field_check = { email, address_l1, address_l2, city, country, postcode, shipping_method_id };
+        const missing_values = Object.keys(field_check).filter(k => !field_check[k].trim());
+        const email_pattern = /^(?:[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+        if (missing_values.length) { res.status(400); throw Error(`Missing fields: ${missing_values.join(", ")}`) }
+        if (!email_pattern.test(email)) { res.status(400); throw Error("Invalid email format") }
+
         var dc_doc = await Discount_code.findOne({ code: discount_code, expiry_date: { $gte: Date.now() } });
         var shipping_method = price_total >= 4000 ? { name: "Free Delivery", fee: 0 } : await Shipping_method.findById(shipping_method_id);
         if (!dc_doc && discount_code) { res.status(404); throw Error("Discount code invalid or expired") }
