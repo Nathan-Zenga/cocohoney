d5420437d91d0adacb06a1787e1d782b05b8c1ee
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-10-01 16:39:18 +0100

    re-apply isAuthenticated method to authcheck module

--- modules/authCheck.js
+++ modules/authCheck.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if (req.session.user) return next();
+    if (req.isAuthenticated() || req.session.user) return next();
     if (req.method === "GET") return res.redirect(req.get("referrer"));
     return res.sendStatus(401);
 }
