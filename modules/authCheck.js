33ff671fa7bb177ccb1910edc496b496d61aa89f
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-10-03 01:26:47 +0100

    redirect to homepage if page access unauthorised

--- modules/authCheck.js
+++ modules/authCheck.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
     if (req.isAuthenticated() || res.locals.user) return next();
-    if (req.method === "GET") return res.redirect(req.get("referrer"));
+    if (req.method === "GET") return res.redirect("/");
     return res.sendStatus(401);
 }
