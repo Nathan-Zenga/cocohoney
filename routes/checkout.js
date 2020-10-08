1cf43968c41f5ff411cac30aad3888c60575cbaf
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-10-08 14:20:06 +0100

    restricting ambassadors from using discount codes on checkout

--- routes/checkout.js
+++ routes/checkout.js
@@ -31,6 +31,7 @@ router.post("/session/create", async (req, res) => {
         const email_pattern = /^(?:[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
         if (missing_values.length) { res.status(400); throw Error(`Missing fields: ${missing_values.join(", ")}`) }
         if (!email_pattern.test(email)) { res.status(400); throw Error("Invalid email format") }
+        if (ambassador && discount_code) { res.status(404); throw Error("This discount code cannot be used as you are an ambassador") }
 
         var dc_doc = await Discount_code.findOne({ code: discount_code, expiry_date: { $gte: Date.now() } });
         var shipping_method = price_total >= 4000 ? { name: "Free Delivery", fee: 0 } : await Shipping_method.findById(shipping_method_id);
