b476ac882e35a6e44e61d0e84e82336508fafc2b
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-10-08 11:13:59 +0100

    set up alternative ambassador verification method

--- routes/ambassador.js
+++ routes/ambassador.js
@@ -70,7 +70,26 @@ router.get('/register/verify', (req, res) => {
         }, err => {
             if (err) return res.status(500).send(err.message);
             amb.verified = true;
-            amb.save(err => res.send(`Registered. Email sent to ${amb.email} for account activation`));
+            amb.save(err => res.send(`${amb.firstname} now verified. Email sent to ${amb.email} for account activation`));
+        });
+    })
+});
+
+router.post('/register/verify', (req, res) => {
+    const { token } = req.body;
+    Ambassador.findOne({ token }, (err, amb) => {
+        if (err) return res.status(500).send(err.message);
+        if (!amb) return res.status(404).send("Invalid entry");
+        new MailingListMailTransporter({ req, res }, { email: amb.email }).sendMail({
+            subject: "Your account is now verified.",
+            message: "Hello,\n\n Your account has been verified and confirmed by " +
+            "the administrator of Cocohoney Cosmetics.\n\n" +
+            "Please click the following link below to activate your account:\n" +
+            `${res.locals.location_origin}/ambassador/register/activate?token=${amb.token}`
+        }, err => {
+            if (err) return res.status(500).send(err.message);
+            amb.verified = true;
+            amb.save(err => res.send(`${amb.firstname} now verified. Email sent to ${amb.email} for account activation`));
         });
     })
 });
