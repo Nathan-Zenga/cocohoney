b94d224e097237b14e2582d24627697903f93df0
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-10-10 15:52:32 +0100

    re-modify output of missing field names

--- routes/checkout-paypal.js
+++ routes/checkout-paypal.js
@@ -21,10 +21,10 @@ router.post("/create-payment", async (req, res) => {
     })).reduce((sum, p) => sum + (p.price * p.quantity), 0);
 
     try {
-        const field_check = { email, address_l1, city, country, postcode, shipping_method_id };
-        const missing_values = Object.keys(field_check).filter(k => !field_check[k]);
+        const field_check = { firstname, lastname, email, address_line1: address_l1, city, country, postcode, shipping_method: shipping_method_id };
+        const missing_fields = Object.keys(field_check).filter(k => !field_check[k]);
         const email_pattern = /^(?:[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
-        if (missing_values.length) { res.status(400); throw Error(`Missing fields: ${missing_values.join(", ")}`) }
+        if (missing_fields.length) { res.status(400); throw Error(`Missing fields: ${missing_fields.join(", ")}`) }
         if (!email_pattern.test(email)) { res.status(400); throw Error("Invalid email format") }
         if (ambassador && discount_code) { res.status(404); throw Error("This discount code cannot be used as you are an ambassador") }
 
