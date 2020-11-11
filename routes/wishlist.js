1babaaa03d283a79966922867736108637920d54
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-11-11 01:07:33 +0000

    restrict admin from adding/removing wishlist items

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -6,6 +6,7 @@ router.post("/add", async (req, res) => {
     const logged_in = req.isAuthenticated() || (user && !is_admin);
     const product = await Product.findById(req.body.id);
     if (!logged_in) return res.status(401).send("Please log in to add items to your wishlist");
+    if (is_admin) return res.status(401).send("Please log in as a customer or ambassador to add wishlist items");
     if (!product) return res.status(404).send("Item does not exist");
     const wishlist = await Wishlist.findOne({ customer_id: user._id }) || new Wishlist({ customer_id: user._id });
     if (!wishlist.items.includes(product.id)) wishlist.items.push(product.id);
@@ -16,6 +17,7 @@ router.post("/remove", async (req, res) => {
     const { user, is_admin, id: product_id } = { ...res.locals, ...req.body };
     const logged_in = req.isAuthenticated() || (user && !is_admin);
     if (!logged_in) return res.status(401).send("Please log in to remove items to your wishlist");
+    if (is_admin) return res.status(401).send("Please log in as a customer or ambassador to remove wishlist items");
     const wishlist = await Wishlist.findOne({ customer_id: user._id });
     if (!wishlist) return res.status(404).send("Wishlist not found");
     wishlist.items = wishlist.items.filter(id => id !== product_id);
