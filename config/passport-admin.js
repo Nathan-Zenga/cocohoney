dcab71dc416f93279c5d8213403076f873ac17ec
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-11-24 21:07:41 +0000

    condense passport config code for admin authentication

--- config/passport-admin.js
+++ config/passport-admin.js
@@ -3,29 +3,26 @@ const { Admin } = require('../models/models');
 const bcrypt = require('bcrypt');
 
 module.exports = passport => {
-    passport.use("local-login-admin", new Strategy({ usernameField: "email" }, (email, password, done) => {
-        Admin.findOne({ email }, (err, user) => {
-            if (err) return done(err);
+    passport.use("local-login-admin", new Strategy({ usernameField: "email" }, async (email, password, done) => {
+        try {
+            const user = await Admin.findOne({ email });
+            const match = bcrypt.compareSync(password, (user || {}).password || "");
             if (!user) return done(null, "to_activate", { message: "Verification email sent" });
-            bcrypt.compare(password, user.password, (err, match) => {
-                if (err) return done(err);
-                if (!match) return done(null, null, { message: "Invalid password" });
-                done(null, user);
-            });
-        });
+            if (!match) return done(null, null, { message: "Invalid password" });
+            done(null, user);
+        } catch (err) { done(err) }
     }));
 
-    passport.use("local-register-admin", new Strategy({ passReqToCallback: true }, (req, email, password, done) => {
-        if (password !== req.body.password_confirm) return done(null, null, { message: "Passwords don't match" });
-        Admin.findOne({ password: req.params.token, token_expiry_date: { $gte: Date.now() } }, (err, found) => {
-            if (!found) return done(null, null, { message: "Cannot activate account: token expired / not valid" });
-            Admin.findOne({ email }, (err, found) => {
-                if (found) return done(null, null, { message: "An admin is already registered" });
-                bcrypt.hash(password, 10, (err, hash) => {
-                    new Admin({ email, password: hash }).save((err, user) => done(null, user));
-                });
-            });
-        });
+    passport.use("local-register-admin", new Strategy({ passReqToCallback: true }, async (req, email, password, done) => {
+        try {
+            if (password !== req.body.password_confirm) return done(null, null, { message: "Passwords don't match" });
+            const applicant = await Admin.findOne({ password: req.params.token, token_expiry_date: { $gte: Date.now() } });
+            const existing = await Admin.findOne({ email });
+            if (!applicant) return done(null, null, { message: "Cannot activate account: expired / invalid token" });
+            if (existing) return done(null, null, { message: "An admin is already registered" });
+            const hash = bcrypt.hashSync(password, 10);
+            new Admin({ email, password: hash }).save((err, user) => done(null, user));
+        } catch (err) { done(err) }
     }));
 
     passport.serializeUser((user, done) => done(null, user.id));
