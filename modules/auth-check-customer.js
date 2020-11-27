a9cfcdcd055dee0910ea8df664621f598e2bab31
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-11-27 14:19:51 +0000

    disable access to customer accounts from ambassadors and admin

--- modules/auth-check-customer.js
+++ modules/auth-check-customer.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if (req.isAuthenticated() || res.locals.user) return next();
+    if (res.locals.user && !res.locals.is_ambassador && !res.locals.is_admin) return next();
     if (req.method === "GET") return res.redirect("/");
     return res.sendStatus(401);
 }
