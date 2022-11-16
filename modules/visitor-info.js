0f01bc5a1191ae8107e1fed3c41c316c48789853
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2022-11-16 15:33:30 +0000

    re-modify elements of box deal & customer review pages
    re-arrange visitor log message

--- modules/visitor-info.js
+++ modules/visitor-info.js
@@ -3,5 +3,5 @@ const uaParser = require('./ua-parser');
 module.exports = (req, res) => {
     const { browser, os, device } = new uaParser(req.headers['user-agent']).getResult();
     const ua = `browser: ${browser.name} ${browser.version}, os: ${os.name} ${os.version}, device: ${device.type}`.replace(/ ?undefined ?/g, "");
-    return `method: ${req.method}, path: ${req.originalUrl}, host: ${req.headers.host}, status: ${res.statusCode} (${res.statusMessage}), ${ua}`;
+    return `method: ${req.method}, status: ${res.statusCode} (${res.statusMessage}), host: ${req.headers.host}, path: ${req.originalUrl}, ${ua}`;
 };
